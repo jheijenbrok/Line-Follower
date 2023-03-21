03ee673a6df4259b3016b776481b07d7c951771c
ardjuh <63558466+ardjuh@users.noreply.github.com>
2023-03-21 17:11:57 +0100

    Use vector instead of 3 different signals (#2)
    * Use vector instead of 3 different signals
    
    * transform input to vector and naming changes
    
    * Fixed inputbuffer shit
    
    ---------
    
    Co-authored-by: Ard Geuze <geuzeard@gmail.com>
    Co-authored-by: Jens Heijenbrok <jensheijenbrok@hotmail.com>

--- inputbuffer.vhdl
+++ inputbuffer.vhdl
@@ -3,36 +3,21 @@ use IEEE.STD_LOGIC_1164.all;
 
 entity three_bit_registry is
     port (
-        clk : in std_logic;
-
-        sensor_l_in : in std_logic;
-        sensor_m_in : in std_logic;
-        sensor_r_in : in std_logic;
-
-        sensor_l_out : out std_logic;
-        sensor_m_out : out std_logic;
-        sensor_r_out : out std_logic
+        clk     : in std_logic;
+        reg_in  : in std_logic_vector(2 downto 0);
+        reg_out : out std_logic_vector(2 downto 0)
     );
 end entity three_bit_registry;
 
 architecture arch of three_bit_registry is
-    signal sensor_l_reg : std_logic_vector(2 downto 0);
-    signal sensor_m_reg : std_logic_vector(2 downto 0);
-    signal sensor_r_reg : std_logic_vector(2 downto 0);
 begin
 
     process (clk)
     begin
-        if (clk'event and clk = '1') then
-            sensor_l_reg <= sensor_l_in & sensor_l_reg(1 downto 0);
-            sensor_m_reg <= sensor_m_in & sensor_m_reg(1 downto 0);
-            sensor_r_reg <= sensor_r_in & sensor_r_reg(1 downto 0);
+        if (rising_edge(clk)) then
+            reg_out <= reg_in;
         end if;
     end process;
-
-    sensor_l_out <= sensor_l_reg(2);
-    sensor_m_out <= sensor_m_reg(2);
-    sensor_r_out <= sensor_r_reg(2);
 
 end architecture arch;
 
@@ -57,42 +42,32 @@ architecture arch of inputbuffer is
 
     component three_bit_registry is
         port (
-            clk : in std_logic;
-
-            sensor_l_in : in std_logic;
-            sensor_m_in : in std_logic;
-            sensor_r_in : in std_logic;
-
-            sensor_l_out : out std_logic;
-            sensor_m_out : out std_logic;
-            sensor_r_out : out std_logic
+            clk     : in std_logic;
+            reg_in  : in std_logic_vector(2 downto 0);
+            reg_out : out std_logic_vector(2 downto 0)
         );
     end component three_bit_registry;
 
-    signal sensor_l_reg : std_logic;
-    signal sensor_m_reg : std_logic;
-    signal sensor_r_reg : std_logic;
+    signal sensor_in, reg1_out, reg2_out : std_logic_vector(2 downto 0);
 
 begin
 
-    LB1 : three_bit_registry port map(
-        clk          => clk,
-        sensor_l_in  => sensor_l_in,
-        sensor_m_in  => sensor_m_in,
-        sensor_r_in  => sensor_r_in,
-        sensor_l_out => sensor_l_reg,
-        sensor_m_out => sensor_m_reg,
-        sensor_r_out => sensor_r_reg
+    sensor_in <= sensor_l_in & sensor_m_in & sensor_r_in;
+
+    reg1: three_bit_registry port map(
+        clk     => clk,
+        reg_in  => sensor_in,
+        reg_out => reg1_out
     );
 
-    LB2 : three_bit_registry port map(
-        clk          => clk,
-        sensor_l_in  => sensor_l_reg,
-        sensor_m_in  => sensor_m_reg,
-        sensor_r_in  => sensor_r_reg,
-        sensor_l_out => sensor_l_out,
-        sensor_m_out => sensor_m_out,
-        sensor_r_out => sensor_r_out
+    reg2: three_bit_registry port map(
+        clk     => clk,
+        reg_in  => reg1_out,
+        reg_out => reg2_out
     );
+
+    sensor_l_out <= reg2_out(2);
+    sensor_m_out <= reg2_out(1);
+    sensor_r_out <= reg2_out(0);
 
 end architecture arch;
