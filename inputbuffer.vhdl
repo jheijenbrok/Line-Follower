3d98572a48da4b7b6565b385e15739d6e1fd626e
Jens Heijenbrok <jensheijenbrok@hotmail.com>
2023-03-21 16:24:30 +0100

    transform input to vector and naming changes

--- inputbuffer.vhdl
+++ inputbuffer.vhdl
@@ -3,9 +3,9 @@ use IEEE.STD_LOGIC_1164.all;
 
 entity three_bit_registry is
     port (
-        clk        : in std_logic;
-        sensor_in  : in std_logic_vector(2 downto 0);
-        sensor_out : out std_logic_vector(2 downto 0)
+        clk     : in std_logic;
+        reg_in  : in std_logic_vector(2 downto 0);
+        reg_out : out std_logic_vector(2 downto 0)
     );
 end entity three_bit_registry;
 
@@ -15,7 +15,7 @@ begin
     process (clk)
     begin
         if (rising_edge(clk)) then
-            sensor_out <= sensor_in;
+            reg_out <= reg_in;
         end if;
     end process;
 
@@ -42,30 +42,32 @@ architecture arch of inputbuffer is
 
     component three_bit_registry is
         port (
-            clk        : in std_logic;
-            sensor_in  : in std_logic_vector(2 downto 0);
-            sensor_out : in std_logic_vector(2 downto 0);
+            clk     : in std_logic;
+            reg_in  : in std_logic_vector(2 downto 0);
+            reg_out : in std_logic_vector(2 downto 0)
         );
     end component three_bit_registry;
 
-    signal sensor_reg, sensor_out : std_logic_vector(2 downto 0);
+    signal sensor_in, sensor_reg, sensor_out : std_logic_vector(2 downto 0);
 
 begin
+
+    sensor_in <= sensor_l_in & sensor_m_in & sensor_r_in;
 
     sensor_l_out <= sensor_out(2);
     sensor_m_out <= sensor_out(1);
     sensor_r_out <= sensor_out(0);
 
     LB1 : three_bit_registry port map(
-        clk        => clk,
-        sensor_in  => sensor_l_in & sensor_m_in & sensor_r_in,
-        sensor_out => sensor_reg
+        clk     => clk,
+        reg_in  => sensor_in,
+        reg_out => sensor_reg
     );
 
     LB2 : three_bit_registry port map(
-        clk        => clk,
-        sensor_in  => sensor_reg,
-        sensor_out => sensor_out
+        clk     => clk,
+        reg_in  => sensor_reg,
+        reg_out => sensor_out
     );
 
 end architecture arch;
